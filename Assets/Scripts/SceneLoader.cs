095cda39b652cc7f8b506d44ac37ee616adc001c
arshavyna <anasbayu03@gmail.com>
2024-01-28 02:50:05 +0700

    credits section & animation

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,11 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject mCredit;
     public void Load(string sceneName){
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 
     public void ExitGame(){
         Application.Quit();
+    }
+
+    public void ShowCredits(){
+        mCredit.SetActive(true);
+    }
+
+    public void CloseCredits(){
+        mCredit.SetActive(false);
     }
 }
